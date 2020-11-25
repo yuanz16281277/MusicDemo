4a9afe452ef1090847acb14f78353cd1a21183a9
zhaoyuan <zhaoyuan202008@e-chinalife.com>
2020-11-25 15:06:32 +0800

    11.25

--- app/src/main/java/com/example/musicdemo/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/musicdemo/activities/WelcomeActivity.java
@@ -2,15 +2,44 @@ package com.example.musicdemo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.musicdemo.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends BaseActivity {
+
+    private Timer mTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        init();
+    }
+
+    private void init() {
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+//                Log.e("WelcomeActivity", "当前线程为：" + Thread.currentThread());
+                toMain();
+            }
+        }, 3 * 1000);
+    }
+
+    /**
+     * 跳转到MainActivity
+     */
+    private void toMain() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
