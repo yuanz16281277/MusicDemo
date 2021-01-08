a42b62429be6a6e07e23dce610c8857a336e9d54
zhaoyuan <zhaoyuan202008@e-chinalife.com>
2021-01-08 09:14:21 +0800

    01.08

--- app/src/main/java/com/example/musicdemo/activities/BaseActivity.java
+++ app/src/main/java/com/example/musicdemo/activities/BaseActivity.java
@@ -1,18 +1,65 @@
 package com.example.musicdemo.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.IdRes;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.example.musicdemo.R;
+
+import java.util.Arrays;
 
 public class BaseActivity extends Activity {
 
     private ImageView mIvBack, mIvme;
     private TextView mTvTitle;
+    private ShortcutManager mShortcutManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            mShortcutManager = getSystemService(ShortcutManager.class);
+            getNewShortcutInfo();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    private void getNewShortcutInfo() {
+        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
+                .setShortLabel("Web site")
+                .setLongLabel("第一个")
+                .setIcon(Icon.createWithResource(this, R.drawable.icon120))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.baidu.com/")))
+                .build();
+        ShortcutInfo shortcut2 = new ShortcutInfo.Builder(this, "id2")
+                .setShortLabel("Web site")
+                .setLongLabel("第二个")
+                .setIcon(Icon.createWithResource(this, R.drawable.icon120))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.csdn.com/")))
+                .build();
+        ShortcutInfo shortcut3 = new ShortcutInfo.Builder(this, "id3")
+                .setShortLabel("Web site")
+                .setLongLabel("第三个")
+                .setIcon(Icon.createWithResource(this, R.drawable.icon120))
+                .setIntent(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.github.com/")))
+                .build();
+        mShortcutManager.setDynamicShortcuts(Arrays.asList(shortcut, shortcut2, shortcut3));
+    }
 
     /**
      * findViewById
