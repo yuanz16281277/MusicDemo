f82f90dcaf14bb381947848235bcbf03f084ca63
yuanz16281277 <16281277@bktu.edu.cn>
2021-02-04 09:44:55 +0800

    test1

--- app/src/main/java/com/example/musicdemo/activities/BaseActivity.java
+++ app/src/main/java/com/example/musicdemo/activities/BaseActivity.java
@@ -33,6 +33,8 @@ public class BaseActivity extends Activity {
             mShortcutManager = getSystemService(ShortcutManager.class);
             getNewShortcutInfo();
         }
+
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N_MR1)
